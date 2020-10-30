7c953d72fb616cc4334d9813b3747c77ca2e42b2
Siddhant kumar Mishra <47280736+Siddhantk123@users.noreply.github.com>
2020-10-30 13:24:00 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -2,28 +2,29 @@ from django.shortcuts import render, HttpResponse, redirect
 import requests
 from bs4 import BeautifulSoup
 import time
-
-# Create your views here.
+import json
+from covid import Covid
 
 def home(request):
+
+    covid = Covid()
+    covid= covid.get_data()
+    # print(covid)
+    df1=[]  #empty list
+    for dic in covid: #dic me each value aayega 
+        df1.append([dic['country'],dic['confirmed'], dic['active'],dic['deaths'],dic['recovered'],dic['last_update']]) #yaha 3-3 ke pair me list me append honge  #ko fetch kr rhe #3-3 ke pair me ye list me add hoge                                                                               #ko fetch kr rhe
+    # print(df1)                                                                            
+    #ab list mil gya us data ka and store ho ya df me
+
+
+    #using the concept of data feame  of pandas
+    import pandas as pd
+    df1=pd.DataFrame(df1,columns=['Country','Confirmed','Active','Deaths','Recoverd','Last Update'])#ye sirf heading dega frame dega
+    #df me table aayega
     
-
-    def webData(url): #Getting The Data From The Website
-        r = requests.get(url)
-        return r.text
-
-    #if __name__ == "__main__":
-        
-    html_doc = webData('https://www.mohfw.gov.in/')
-    soup = BeautifulSoup(html_doc, 'html.parser') #Parsing The Data
-            
-
-    x= soup.find_all ('table')
-    exc={'table':str (x)}
-    #data=exc['table']
-            
-    return render(request, 'home.htm',exc) 
-            
+    exc1={'table0':df1.to_html()}
+    #print(exc1)
+    return render(request, 'home.htm',exc1) 
             
 
 
